41aeaf0aaf82dce4fa9aee36e1206e506af81d69
MiruthyanJayanS <miruthya.s.ad.2020@snsce.ac.in>
2025-08-25 12:59:13 +0530

    Changes made to the file

--- Sample.java
+++ Sample.java
@@ -2,5 +2,7 @@ class Sample{
 	public static void main(String...args){
 		System.out.println("Hello Java");
 		System.out.println("Changes Made");
+		
+		System.out.println("Changes Made second time");
 	}
 }
